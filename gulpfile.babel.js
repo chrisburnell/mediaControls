ffceb4f1cf981276e740357b836581208bf24e91
Chris Burnell <me@chrisburnell.com>
2020-01-10 20:15:49 +0000

    build overhaul + native controls

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,112 +4,140 @@
  * @version 1.1.2
  */
 
-
-'use strict';
-
+"use strict";
 
 // Define gulp-centric objects
-let gulp = require('gulp');
-let babel = require('gulp-babel');
-let eslint = require('gulp-eslint');
-let newer = require('gulp-newer');
-let plumber = require('gulp-plumber');
-let postcss = require('gulp-postcss');
-let rename = require('gulp-rename');
-let sass = require('gulp-sass');
-let watch = require('gulp-watch');
+let gulp = require("gulp");
+let babel = require("gulp-babel");
+let concat = require("gulp-concat");
+let csso = require("gulp-csso");
+let newer = require("gulp-newer");
+let plumber = require("gulp-plumber");
+let postcss = require("gulp-postcss");
+let prettier = require("@bdchauvette/gulp-prettier");
+let rename = require("gulp-rename");
+let sass = require("gulp-sass");
+let watch = require("gulp-watch");
 
 // Define other objects
-let autoprefixer = require('autoprefixer');
-let cssnano = require('cssnano');
-let reporter = require('postcss-reporter');
-let scss_syntax = require('postcss-scss');
+let autoprefixer = require("autoprefixer");
 
 // Define paths
 const paths = {
-    root: '.',
-    src: 'src',
-    includes: '_includes'
+    root: ".",
+    css: {
+        src: "src",
+        dest: "."
+    },
+    js: {
+        src: "src",
+        dest: "."
+    },
+    includes: "_includes"
 };
 
 // -----------------------------------------------------------------------------
 
-// Compile CSS from Sass
-gulp.task('css-main', () => {
-    return gulp.src([`${paths.src}/main.scss`])
+// Prettify Sass
+gulp.task("css-prettify", () => {
+    return gulp
+        .src([
+            `${paths.css.src}/**/*.scss`
+        ])
         .pipe(plumber())
-        .pipe(sass({
-            errLogToConsole: true,
-            indentWidth: 4,
-            outputStyle: 'expanded'
-        }))
-        .pipe(postcss([
-            autoprefixer(),
-            cssnano(),
-            reporter({
-                plugins: ['!postcss-discard-empty'],
-                clearMessages: true,
-                throwError: true
+        .pipe(newer(`${paths.css.src}`))
+        .pipe(
+            prettier({
+                printWidth: 9999,
+                tabWidth: 4
             })
-        ]))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(`${paths.root}/`));
+        )
+        .pipe(gulp.dest(`${paths.css.src}/`));
+});
+
+// Compile CSS from Sass
+gulp.task("css-compile", () => {
+    return gulp
+        .src([
+            `${paths.css.src}/main.scss`
+        ])
+        .pipe(plumber())
+        .pipe(
+            sass({
+                errLogToConsole: true,
+                indentWidth: 4,
+                outputStyle: "expanded"
+            })
+        )
+        .pipe(postcss([autoprefixer()]))
+        .pipe(
+            prettier({
+                printWidth: 9999,
+                tabWidth: 4
+            })
+        )
+        .pipe(gulp.dest(`${paths.css.dest}/`))
+        .pipe(csso())
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(gulp.dest(`${paths.css.dest}/`));
 });
 
 // -----------------------------------------------------------------------------
 
-// Lint JavaScript
-gulp.task('js-lint', ['js-transport'], () => {
-    return gulp.src([`${paths.includes}/mediaControls.min.js`])
+// Prettify JavaScript
+gulp.task("js-prettify", () => {
+    return gulp
+        .src([
+            `${paths.js.src}/**/*.js`
+        ])
         .pipe(plumber())
-        .pipe(eslint({
-            fix: true
-        }))
-        .pipe(eslint.format())
-        .pipe(gulp.dest(`${paths.includes}/`));
+        .pipe(newer(`${paths.js.src}/`))
+        .pipe(
+            prettier({
+                printWidth: 9999,
+                tabWidth: 4
+            })
+        )
+        .pipe(gulp.dest(`${paths.js.src}/`));
 });
 
-// Transport JavaScript
-gulp.task('js-transport', () => {
-    return gulp.src([`${paths.src}/mediaControls.js`])
+// Concatenate JavaScript
+gulp.task("js-concat", () => {
+    return gulp
+        .src([
+            `${paths.js.src}/**/*.js`
+        ])
         .pipe(plumber())
-        .pipe(gulp.dest(`${paths.root}/`))
+        .pipe(concat("main.js"))
+        .pipe(gulp.dest(`${paths.js.dest}/`))
         .pipe(babel())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(`${paths.root}/`))
-        .pipe(gulp.dest(`${paths.includes}/`));
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(gulp.dest(`${paths.js.dest}/`));
 });
 
 // -----------------------------------------------------------------------------
-
-// Default task
-gulp.task('default', () => {
-    gulp.start('css');
-    gulp.start('js');
-});
 
 // CSS task
-gulp.task('css', () => {
-    gulp.start('css-main');
-});
+gulp.task("css", gulp.series("css-prettify", "css-compile"));
 
 // JS task
-gulp.task('js', () => {
-    gulp.start('js-lint');
-});
+gulp.task("js", gulp.series("js-concat"));
+
+// Default task
+gulp.task("default", gulp.parallel("css", "js"));
 
 // -----------------------------------------------------------------------------
 
 // Watch files and perform the appropriate tasks
-gulp.task('watch', ['css', 'js'], () => {
-    watch(`${paths.root}/src/*.scss`, () => {
-        gulp.start('css');
-    });
-    watch(`${paths.root}/src/*.js`, () => {
-        gulp.start('js');
-    });
+gulp.task("watch", () => {
+    gulp.watch(`${paths.css.src}/**/*`, gulp.series("css"));
+    gulp.watch(`${paths.js.src}/**/*`, gulp.series("js"));
 });
